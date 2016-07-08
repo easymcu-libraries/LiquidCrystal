e8e3192d73448112cd67f49702396d7551ea1eb1
Morteza Zandi <mzandi@users.noreply.github.com>
2016-07-08 15:16:06 +0430

    Update Display.cpp

--- Examples/Display.cpp
+++ Examples/Display.cpp
@@ -1,15 +1,15 @@
 	#include "headers.h"
-
-
+	
+	
 	// initialize the library with the numbers of the interface pins
 	LiquidCrystal lcd(P40, P39, P38, P37, P36, P35);
 
-  int main()
+  	int main()
 	{
-			// set up the LCD's number of columns and rows: 
-			lcd.begin(16, 2);
-			// Print a message to the LCD.
-			lcd.print("hello, world!");
+		// set up the LCD's number of columns and rows: 
+		lcd.begin(16, 2);
+		// Print a message to the LCD.
+		lcd.print("hello, world!");
 
 		while(1)
 		{
